5d265b27803a7fd245c10274773ac9b0bcf65892
Nana K <73991180+topboyasante@users.noreply.github.com>
2024-04-02 08:50:33 +0000

    routing

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import * as bcrypt from "bcrypt";
 import NextAuth from "next-auth/next";
 import { User } from "@prisma/client";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   pages: {
     signIn: "/sign-in",
   },
